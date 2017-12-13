4e34d5a2e2f7b0890bfa48919f88eef7d13cc763
joyin <1036936157@qq.com>
2017-12-13 11:21:40 +0800

    Update User.java

---
+++ com/join/model/User.java
@@ -0,0 +1,20 @@
+public Class User{
+    private String userName;
+    private Integer userAge;
+  
+    public void setUserName(String userName) {
+        this.userName = userName;  
+    }
+    
+    public String getUserName() {
+        return this.userName;
+    }
+    
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+    
+    public String getUserAge() {
+        return this.userAge;
+    }
+}
